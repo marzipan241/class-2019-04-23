cfd1c86907823ab225eacf7d1003ef5810607ca8
Margaret  Sun <MargaretSun@MacBook-Pro-8.local>
2019-04-23 13:47:28 -0400

    Edit main panel

--- class-2019-04-23/app.R
+++ class-2019-04-23/app.R
@@ -42,6 +42,7 @@ server <- function(input, output) {
   
   output$selected_var <- renderText({ 
     paste("You have selected", input$var)
+    paste("You have chosen a range that goes from", input$range[1], "to", input$range[2])
   })
   
 }
