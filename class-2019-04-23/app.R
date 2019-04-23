ad0ce8808f17fd9a5ecba867d3a73580f0de8b2f
Margaret  Sun <MargaretSun@MacBook-Pro-8.local>
2019-04-23 13:56:02 -0400

    Edited Shiny app to be responsive to slider

--- class-2019-04-23/app.R
+++ class-2019-04-23/app.R
@@ -1,15 +1,5 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
   titlePanel("censusVis"),
   
@@ -29,24 +19,26 @@ ui <- fluidPage(
       sliderInput("range", 
                   label = "Range of interest:",
                   min = 0, max = 100, value = c(0, 100))
-    ),
+      ),
     
     mainPanel(
-      textOutput("selected_var")
+      textOutput("selected_var"),
+      textOutput("min_max")
     )
   )
-)
+  )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   
   output$selected_var <- renderText({ 
     paste("You have selected", input$var)
-    paste("You have chosen a range that goes from", input$range[1], "to", input$range[2])
+  })
+  
+  output$min_max <- renderText({ 
+    paste("You have chosen a range that goes from",
+          input$range[1], "to", input$range[2])
   })
   
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
-
+shinyApp(ui, server)
